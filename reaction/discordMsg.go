bb2b084ee7aeaa1017d0ae65ce9defef1f2d88d3
Valiance <146172201+LucasLudovic@users.noreply.github.com>
2025-12-09 17:43:37 +0100

    feat: add discord message reaction (#44)
    * feat: add discord msg reaction
    
    * feat: bind reaction to action

---
+++ reaction/discordMsg.go
@@ -0,0 +1,43 @@
+package reaction
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/ValianceTekProject/AreaBack/db"
+	"github.com/ValianceTekProject/AreaBack/model"
+)
+
+func ReactWithDiscordMsg(user db.UsersModel) {
+	var config model.DiscordConfig
+
+	config.WebhookURL = "https://discord.com/api/webhooks/1447899360145965127/NNlPLGO08_jbI1Pz0SED7wFqtsYvK4H93xG_v6Z25VZEuDd090IkvfpicUOFak9U5bPz"
+
+	if config.WebhookURL == "" {
+		fmt.Println("Error in webhook url")
+		return
+	}
+
+	payload := model.DiscordMessage{
+		Content: "PR Merge",
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("Error unmarshaling message: %s\n", err.Error())
+		return
+	}
+
+	resp, err := http.Post(config.WebhookURL, "application/json", bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		fmt.Printf("Error: %s\n", resp.Status)
+	}
+}
