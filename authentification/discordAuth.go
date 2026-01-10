7215c535c9d68d1bf0de1df57f9e936d2fb5f421
Lucas <lucas.ludovic@epitech.eu>
2026-01-10 10:05:05 +0100

    fix: redirect to front or mobile

--- authentification/discordAuth.go
+++ authentification/discordAuth.go
@@ -185,6 +185,12 @@ func DiscordLogin(ctx *gin.Context) {
 	if exist {
 		ctx.SetCookie("oauth_user_id", userID.(string), 300, "/", "", false, true)
 	}
+
+	redirectURI := ctx.Query("redirect_uri")
+	if redirectURI != "" {
+		ctx.SetCookie("oauth_redirect_uri", redirectURI, 300, "/", "", false, true)
+	}
+
 	ctx.SetCookie("oauthState", oauthState, 3600, "/", "", false, true)
 
 	url := discordOauthConfig.AuthCodeURL(oauthState, oauth2.AccessTypeOffline)
@@ -232,14 +238,23 @@ func DiscordCallback(ctx *gin.Context) {
 		return
 	}
 
+	customRedirectURI, err := ctx.Cookie("oauth_redirect_uri")
+	if err == nil && customRedirectURI != "" {
+		ctx.SetCookie("oauth_redirect_uri", "", -1, "/", "", false, true)
+	}
+
 	frontendURL := os.Getenv("FRONTEND_URL")
 	if frontendURL == "" {
 		frontendURL = "http://localhost:8081"
 	}
 
 	if userIDPtr != nil && *userIDPtr != "" {
-		redirectURL := fmt.Sprintf("%s/dashboard", frontendURL)
-		ctx.Redirect(http.StatusTemporaryRedirect, redirectURL)
+		if customRedirectURI != "" {
+			ctx.Redirect(http.StatusTemporaryRedirect, customRedirectURI)
+		} else {
+			redirectURL := fmt.Sprintf("%s/dashboard", frontendURL)
+			ctx.Redirect(http.StatusTemporaryRedirect, redirectURL)
+		}
 	} else {
 		tokenJWT, err := GenerateJWT(user.ID)
 		if err != nil {
@@ -247,7 +262,12 @@ func DiscordCallback(ctx *gin.Context) {
 			return
 		}
 
-		redirectURL := fmt.Sprintf("%s/login?token=%s", frontendURL, tokenJWT)
-		ctx.Redirect(http.StatusTemporaryRedirect, redirectURL)
+		if customRedirectURI != "" {
+			redirectURL := fmt.Sprintf("%s?token=%s", customRedirectURI, tokenJWT)
+			ctx.Redirect(http.StatusTemporaryRedirect, redirectURL)
+		} else {
+			redirectURL := fmt.Sprintf("%s/login?token=%s", frontendURL, tokenJWT)
+			ctx.Redirect(http.StatusTemporaryRedirect, redirectURL)
+		}
 	}
 }
